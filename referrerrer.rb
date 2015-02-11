906142f3bf85445467781af4c35c2aa12306e8e2
Neil Matatall <oreoshake@github.com>
2015-02-11 11:52:30 -0800

    setup endpoints for all possible values (+never)

--- referrerrer.rb
+++ referrerrer.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
 
-get '/' do
-  "Hello World!"
+['no-referrer', 'never', 'origin', 'origin-when-crossorigin', 'no-referrer-when-downgrade', 'unsafe-url'].each do |content|
+  get "/#{content}" do
+    "<meta name=\"referrer\" content=\"#{content}\"><a href=\"/#{content}\">self</a><br>Referrer: #{request.referrer}"
+  end
 end
