b96ed90b6f5fb9a9d26e07e70ce0457f7a39c47c
Neil Matatall <oreoshake@github.com>
2015-02-11 12:08:10 -0800

    cleanup

--- referrerrer.rb
+++ referrerrer.rb
@@ -10,9 +10,9 @@ DIRECTIVE_VALUES.each do |content|
     <meta name="referrer" content="#{content}" />
   </head>
   <body>
-    <a href="/#{content}">self</a><a href="http://bar.com">http://bar.com</a><a href="https://google.com">https://google.com</a><br />
-    The default value for browser behavior is <a href="https://w3c.github.io/webappsec/specs/referrer-policy/#referrer-policy-state-no-referrer-when-downgrade">"no-referrer-when-downgrade".</a>
-    Referrer: #{request.referrer}<br />
+    <p><a href="/#{content}">self</a> <a href="http://bar.com">http://bar.com</a> <a href="https://google.com">https://google.com</a></p>
+    <p>The default value for browser behavior is "<a href="https://w3c.github.io/webappsec/specs/referrer-policy/#referrer-policy-state-no-referrer-when-downgrade">no-referrer-when-downgrade</a>".</p>
+    <p>Referrer: #{request.referrer}</p>
     #{DIRECTIVE_VALUES.map {|value| "<a href=\"#{value}\">#{value}</a>"}.join(" | ")}
   </body>
 HTML
