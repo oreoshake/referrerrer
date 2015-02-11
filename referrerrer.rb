73f32bdd41b433e270900862df5f35045aeca473
Neil Matatall <oreoshake@github.com>
2015-02-11 12:01:19 -0800

    add more information about the values

--- referrerrer.rb
+++ referrerrer.rb
@@ -1,7 +1,20 @@
 require 'sinatra'
 
-['no-referrer', 'never', 'origin', 'origin-when-crossorigin', 'no-referrer-when-downgrade', 'unsafe-url'].each do |content|
+DIRECTIVE_VALUES = %w{no-referrer origin origin-when-crossorigin no-referrer-when-downgrade unsafe-url}
+
+DIRECTIVE_VALUES.each do |content|
   get "/#{content}" do
-    "<meta name=\"referrer\" content=\"#{content}\"><a href=\"/#{content}\">self</a><br>Referrer: #{request.referrer}"
+    <<-HTML
+<html>
+  <head>
+    <meta name="referrer" content="#{content}" />
+  </head>
+  <body>
+    <a href="/#{content}">self</a><a href="http://bar.com">http://bar.com</a><a href="https://google.com">https://google.com</a><br />
+    The default value for browser behavior is <a href="https://w3c.github.io/webappsec/specs/referrer-policy/#referrer-policy-state-no-referrer-when-downgrade">"no-referrer-when-downgrade".</a>
+    Referrer: #{request.referrer}<br />
+    #{DIRECTIVE_VALUES.map {|value| "<a href=\"#{value}\">#{value}</a>"}.join(" | ")}
+  </body>
+HTML
   end
 end
